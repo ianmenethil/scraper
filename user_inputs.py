1af18b7a4abce68703e39880c2c1772a372ea65f
Ian Menethil <62359909+ianmenethil@users.noreply.github.com>
2023-08-17 23:02:44 +1000

    master

--- user_inputs.py
+++ user_inputs.py
@@ -4,12 +4,10 @@ def get_choice():
 
 def auto_flow():
     default_wait_time = 1200  # 20 minutes
-    wait_time = input(
-                        """
-                        The default timer for emails is 20 minutes.
-                        Press enter to use the default timer or enter a new timer in minutes:
-                        """)
-    print("User entered: {wait_time}")
+    wait_time = input("""
+                      The default timer for emails is 20 minutes.
+                      Press enter to use the default timer or enter a new timer in minutes:""")
+    print(f"User entered: {wait_time}")
     wait_time = default_wait_time if wait_time == "" else int(wait_time) * 60
     choice = '2'
     return wait_time, choice
